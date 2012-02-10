418447a14352b88aaf62f4da287b8fc8efa96c58
Javier Ayala <jav.forzza@gmail.com>
2012-02-10 10:42:33 -0600

    cambios

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://192.168.0.115/extrapoints/';
+$config['base_url']	= 'http://10.20.20.166/extrapoints/';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,42 +1,38 @@
-<div id="container">
-	<h1>EXTRAPOINTS</h1>
-
-	<div id="body">
-		<p>Hola <strong><?php echo $username; ?></strong>!</p>
-		
-		<p>Bienvenido a extrapoints, el mejor lugar para compartir tus puntos y ganar premios.</p>
-		
-		<p>level : <?php echo $this->tank_auth->get_user_level()?></p>
-		
-		<?php echo anchor('/upload', 'Importa lista de estudiantes'); ?>
-		
-		<h3>Alumnos</h3>
-		
-		<table>
-		
-		<tr>
-			<td  style="padding:15px;"><strong>NOMBRE</strong></td>
-			<td  style="padding:15px;"><strong>APELLIDO PATERNO</strong></td>
-			<td  style="padding:15px;"><strong>APELLIDO MATERNO</strong></td>
-			<td  style="padding:15px;"><strong>FECHA DE NACIMIENTO</strong></td>
-			<td  style="padding:15px;"><strong>PUNTOS</strong></td>
-		</tr>
-		<?php 
-		
-			foreach($alumnos->result() as $alumno){
-				echo "<tr>";
-					echo "<td>".$alumno->nombre."</td>";
-					echo "<td>".$alumno->apellido_p."</td>";
-					echo "<td>".$alumno->apellido_m."</td>";
-					echo "<td>".$alumno->fechaNac."</td>";
-					echo "<td>".$alumno->puntos."</td>";
-				echo "</tr>";
-			}
+<div class="unit">
+	<div class="container wrapper welcome">
+		<h1>EXTRAPOINTS</h1>
+	
+		<div id="body">
+			<p>Hola <strong><?php echo $username; ?></strong>!</p>
 			
-		?>
-		</table>
-		
+			<p>Bienvenido a extrapoints, el mejor lugar para compartir tus puntos y ganar premios.</p>
+			
+			<p>level : <?php echo $this->tank_auth->get_user_level()?></p>
+			
+			<?php echo anchor('/upload', 'Importa lista de estudiantes'); ?>
+			
+			<div class="unit">
+				<div class="container lista-alumnos">
+					
+					<h3>Alumnos</h3>
+					
+					<?php 
+			
+						foreach($alumnos->result() as $alumno){
+							echo '<div class="columns on-4"><div class="container alumn">';
+								echo	'<div class="column span-3">'.$alumno->nombre.' '.$alumno->apellido_p.' '.$alumno->apellido_m.'<p>'.$alumno->fechaNac.'</p></div>';
+								echo	'<div class="column">'.$alumno->puntos.' Pts. </div>';
+							echo "</div></div>";
+						}
+						
+					?>
+					
+					
+				</div>
+			</div>
+			
+		</div>
+	
+		<p class="footer"><?php echo anchor('/auth/change_password/', 'Cambiar password'); ?> | &iquest;Te quieres ir? Pues  <?php echo anchor('/auth/logout/', 'salte'); ?></p>
 	</div>
-
-	<p class="footer"><?php echo anchor('/auth/change_password/', 'Cambiar password'); ?> | &iquest;Te quieres ir? Pues  <?php echo anchor('/auth/logout/', 'salte'); ?></p>
 </div>
