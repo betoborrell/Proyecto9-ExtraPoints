4059f2e1c6e155f2d9399c0ac3001d9abb8aa0d1
Borrell <beto.borrell@gmail.com>
2012-02-10 10:43:25 -0600

    leve

--- application/config/config.php
+++ application/config/config.php
@@ -15,7 +15,7 @@
 |
 */
 //<<<<<<< HEAD
-$config['base_url']	= 'http://localhost/Proyecto9/';
+$config['base_url']	= '/Proyecto9/';
 //=======
 //$config['base_url']	= 'http://192.168.0.115/extrapoints/';
 //>>>>>>> upstream/master
