76334ccce5718742ee558f38b844f4d09a39c478
Javier Ayala <jav.forzza@gmail.com>
2012-02-17 16:52:47 -0600

    disque hay cambios

--- application/views/upload_success.php
+++ application/views/upload_success.php
@@ -1,11 +1,13 @@
 <div class="unit">
 	<div class="container wrapper">
 		<h3>Se importo el archivo correctamente!</h3>
-
-		<pre>
-			<?php print_r($csvData);?> 
-		</pre>
 		
 		<p><?php echo anchor('upload', 'Importa otra lista!'); ?></p>
 	</div>
 </div>
+
+<script type="text/javascript">
+	setTimeout(function() {
+	  window.location.href = "<?= base_url(); ?>";
+	}, 4000);
+</script>
